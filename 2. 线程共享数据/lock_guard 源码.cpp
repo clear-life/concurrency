397a20c007d2a76ef24f11c8f520af03d7c5fb9f
clearlife <1336509100@qq.com>
2022-10-26 23:33:29 +0800

    多线程

--- 2. 线程共享数据/lock_guard 源码.cpp
+++ 2. 线程共享数据/lock_guard 源码.cpp
@@ -1,29 +1,25 @@
-#include <mutex>
-
 template <class T>
 class lock_guard 
 { 
 public:
-    using mutex_type = T;
-
     // 构造加锁
-    explicit lock_guard(T& _Mtx) : _MyMutex(_Mtx)   // 参数为 mutex 
+    explicit lock_guard(T& _Mtx) : m(_Mtx)   // 参数为 mutex 
     {
-        _MyMutex.lock();
+        m.lock();
     }
 
     // 不上锁
-    lock_guard(T& _Mtx, adopt_lock_t) : _MyMutex(_Mtx) {}   // std::adopt_lock 是 std::adopt_lock_t 类的实例
+    lock_guard(T& _Mtx, adopt_lock_t) : m(_Mtx) {}   // std::adopt_lock 是 std::adopt_lock_t 类的实例
 
     // 析构解锁
     ~lock_guard() noexcept 
     {
-        _MyMutex.unlock();
+        m.unlock();
     }
 
     lock_guard(const lock_guard&) = delete;
     lock_guard& operator=(const lock_guard&) = delete;
 
 private:
-    T& _MyMutex;
+    T& m;
 };
